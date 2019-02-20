27d03768dddc4485eeb2a608a68c7e265b8b4f9f
snitch <sniitch99@gmail.com>
2019-02-20 11:14:31 +0100

    app lato prof (appello)

---
+++ Profside1/app/src/main/java/com/example/snitch/profside1/Registro.java
@@ -0,0 +1,13 @@
+package com.example.snitch.profside1;
+
+public class Registro
+{
+    private Studente classe[];
+
+    public Registro(Studente[] classe)
+    {
+        this.classe = new Studente[50];
+    }
+
+
+}
