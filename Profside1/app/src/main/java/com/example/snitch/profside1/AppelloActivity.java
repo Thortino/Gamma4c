b0965f1dc082cd1129a08bb856641a92654b29f4
snitch <sniitch99@gmail.com>
2019-02-22 09:03:13 +0100

    app lato prof (appello)

--- Profside1/app/src/main/java/com/example/snitch/profside1/AppelloActivity.java
+++ Profside1/app/src/main/java/com/example/snitch/profside1/AppelloActivity.java
@@ -54,7 +54,7 @@ public class AppelloActivity extends AppCompatActivity
 
                 for(int i=0;i<2;i++)
                 {
-                    if(device.getAddress().equals(reg.classe[i].getMac())
+                    if(device.getAddress().equals(reg.classe[i].getMac()))
                         mDeviceList.add(reg.classe[i].getName()+"\n"+device.getAddress());
                 }
             }
